1b3929b8e025df749b9aa1de5d49f8a187394e57
raxit-Cybersync <raxit@cybersync-technologies.com>
2026-03-04 20:27:47 +0530

    express routing

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-import express from "express"
+import express, { response } from "express"
 
 const app = express();
 
+app.use(express.json());
 
+
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+})
+
+app.get("/api/data",(req,res)=>{
+    res.json({message:"Welcone to Data API"},{response:"Success"});
+})
+
+app.post("/users",(req,res)=>{
+    let body = req.body;
+    res.send(body)
+})
 
 
 app.listen(3000, () => {});
